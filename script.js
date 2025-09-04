a378e2c478b62f21b8b855bf58ca1a9958e28ab0
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-09-04 16:00:56 -0700

    Generate Odds button works still working on styling inputs

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let addPlayers = document.querySelector('#create')
+document.querySelector('#btn').addEventListener('click', inputPlayers)
+
+function inputPlayers() {
+    // get number of teams
+    let number = parseInt(document.querySelector('#numTeams').value);
+
+    // clear old inputs
+    const container = document.querySelector('#teamInputs');
+    container.innerHTML = '';
+
+    // create one input for each team
+    for (let i = 0; i < number; i++) {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = `Team ${i + 1} Name`;
+        input.id = `teamNames`;
+        container.appendChild(input);
+    }
+}
