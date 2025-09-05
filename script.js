a0f839d61bf2e1ec705d5b329aedbaab5f6f52ba
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-09-04 17:09:58 -0700

    got inputs to show after generate button

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 document.querySelector('#btn').addEventListener('click', inputPlayers)
 
 function inputPlayers() {
-    // get number of teams
     let number = parseInt(document.querySelector('#numTeams').value);
-
-    // clear old inputs
     const container = document.querySelector('#teamInputs');
     container.innerHTML = '';
 
-    // create one input for each team
     for (let i = 0; i < number; i++) {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'teamWrapper';  // new container for flex styling
+
+        players = document.querySelector('h2').innerHTML = "Who's All Playing"
+
         const input = document.createElement('input');
         input.type = 'text';
         input.placeholder = `Team ${i + 1} Name`;
         input.id = `teamNames`;
-        container.appendChild(input);
+
+        wrapper.appendChild(input);
+        container.appendChild(wrapper);
     }
 }
